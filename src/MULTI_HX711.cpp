c6b3b021c23def84485b4a947dbaaa731523ea1c
niederschelden <142839151+niederschelden@users.noreply.github.com>
2024-03-04 15:25:34 +0100

    work work work

--- src/MULTI_HX711.cpp
+++ src/MULTI_HX711.cpp
@@ -11,6 +11,34 @@ MULTI_HX711::MULTI_HX711(byte* output_pins, byte* clock_pins, byte num_out, byte
   init(output_pins, clock_pins, num_out, num_clk); // Rufe die init-Methode mit den übergebenen Arrays auf
 }
 
+void MULTI_HX711::init(byte* output_pins, byte* clock_pins, byte num_out, byte num_clk) {
+  // Initialisierung der Variablen
+  GAIN = 1;
+  
+  // Speichere die Anzahl der Outpins und Clockpins
+  this->num_out = num_out;
+  this->num_clk = num_clk;
+
+  // Allokiere Speicher für die Arrays und kopiere die Werte
+  CLOCK_PINS = new byte[num_clk];
+  OUT_PINS = new byte[num_out];
+  for (byte i = 0; i < num_clk; i++) {
+    CLOCK_PINS[i] = clock_pins[i];
+  }
+  for (byte i = 0; i < num_out; i++) {
+    OUT_PINS[i] = output_pins[i];
+  }
+
+  // Konfiguriere die Pins, falls noch nicht geschehen
+  for (byte i = 0; i < num_clk; i++) {
+      pinMode(CLOCK_PINS[i], OUTPUT);
+    }
+  for (byte i = 0; i < num_out; i++) {
+      pinMode(OUT_PINS[i], INPUT);
+    }
+  pinsConfigured = true;
+  }
+
 MULTI_HX711::~MULTI_HX711() {
   // Freigabe des allokierten Speichers für die Arrays
   delete[] CLOCK_PINS;
@@ -18,13 +46,14 @@ MULTI_HX711::~MULTI_HX711() {
 }
 
 bool MULTI_HX711::readyToSend() {
-  if (!pinsConfigured) {
-    // We need to set the pin mode once, but not in the constructor
-    pinMode(CLOCK_PIN, OUTPUT);
-    pinMode(OUT_PIN, INPUT);
-    pinsConfigured = true;
+  // Iteriere durch alle Output-Pins
+  for (byte i = 0; i < num_out; i++) {
+    // Prüfe, ob der aktuelle Output-Pin nicht bereit ist
+    if (digitalRead(OUT_PINS[i]) != LOW) {
+      return false; // Wenn nicht bereit, gebe false zurück
+    }
   }
-  return digitalRead(OUT_PIN) == LOW;
+  return true; // Wenn alle bereit sind, gebe true zurück
 }
 
 void MULTI_HX711::setGain(byte gain) {
@@ -38,27 +67,50 @@ void MULTI_HX711::setGain(byte gain) {
     case 32:
       GAIN = 2;
       break;
+    default:
+      // Wenn ein ungültiger Wert übergeben wird, setze GAIN auf den Standardwert (1)
+      GAIN = 1;
+      break;
   }
 
-  digitalWrite(CLOCK_PIN, LOW);
+  // Iteriere durch alle Clock-Pins und setze sie auf LOW
+  for (byte i = 0; i < num_clk; i++) {
+    digitalWrite(CLOCK_PINS[i], LOW);
+  }
+
+  // Führe die read()-Methode aus
   read();
 }
 
-long MULTI_HX711::read() {
-   while (!readyToSend());
+uint32_t* MULTI_HX711::read() {
+  while (!readyToSend());
 
-  byte data[3];
+  // Array zum Speichern der Daten für jeden Data-Pin
+  uint32_t* data = new uint32_t[num_out];
 
-  for (byte j = 3; j--;) {
-      data[j] = shiftIn(OUT_PIN,CLOCK_PIN, MSBFIRST);
+  for (int i = 0; i < GAIN + 24; i++) {
+    // Schalte alle Clock-Pins gleichzeitig um
+    for (byte j = 0; j < num_clk; j++) {
+      digitalWrite(CLOCK_PINS[j], HIGH);
+    }
+    delayMicroseconds(1); // Optional: Wartezeit für schnelle Mikrocontroller
+    for (byte j = 0; j < num_clk; j++) {
+      digitalWrite(CLOCK_PINS[j], LOW);
+    }
+    delayMicroseconds(1); // Optional: Wartezeit für schnelle Mikrocontroller
+    
+    // Lies die Daten von den Input-Pins und speichere sie im entsprechenden Datenarray
+    if (i < 24) {
+      for (byte j = 0; j < num_out; j++) {
+        data[j] = (data[j] << 1) | digitalRead(OUT_PINS[j]);
+      }
+    }
   }
 
-  // set gain
-  for (int i = 0; i < GAIN; i++) {
-    digitalWrite(CLOCK_PIN, HIGH);
-    digitalWrite(CLOCK_PIN, LOW);
+  // Flippe Bit 23 in jedem Datenwert
+  for (byte j = 0; j < num_out; j++) {
+    data[j] ^= 0x800000;
   }
 
-  data[2] ^= 0x80;
-  return ((uint32_t) data[2] << 16) | ((uint32_t) data[1] << 8) | (uint32_t) data[0];
+  return data;
 }
